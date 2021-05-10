3fb7ccd67e702affecb14166b5f08ff809280014
tanbre <tannor@breitigam.email>
2021-05-10 11:36:41 -0700

    added to the about page

---
+++ src/components/FlexImages.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../styles/FlexImages.css'
+
+
+const FlexImages = ({ images }) => {
+    return (
+        <div className='flex-images'>
+            FlexImages
+        </div>
+    )
+}
+
+
+export default FlexImages
