445ec602072e99fc28165928271d9f36eec660f8
tanbre <tannor@breitigam.email>
2021-05-14 15:21:24 -0700

    added graphQL query to identify slugs dynamically

--- src/components/header.js
+++ src/components/header.js
@@ -4,19 +4,11 @@ import '../styles/header.css';
 import NavMenu from '../components/nav-menu';
 
 
-const Header = ({ path }) => {
-    const options = [
-        'about',
-        'contact',
-        'projects',
-        'resume',
-        'technologies',
-    ];
-
+const Header = ({ path, navOptions }) => {
     return (
         <div className='header'>
             <h1 className='header__name'>Tannor Breitigam</h1>
-            <NavMenu options={options} path={path}/>
+            <NavMenu navOptions={navOptions} path={path}/>
         </div>
     );
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import Helmet from "react-helmet"
+import { graphql, useStaticQuery } from 'gatsby'
 
 import '../styles/layout.css';
 import Header from './header';
 import Footer from './footer';
 
 const Layout = ({ children, location }) => {
+    const allSlugs = useStaticQuery(graphql`query allSlugs {
+        allSitePage(filter: {path: {nin: ["/dev-404-page/", "/404.html", "/404/", "/"]}}) {
+          edges {
+            node {
+              path
+            }
+          }
+        }
+      }`);
+
+    const navOptions = allSlugs.allSitePage.edges.map(edge => edge.node.path.split('/').join(''));
+
     let path = location.pathname.split('/').join('');
-    if (path === '404.html') {
-        path = '404'
-    }
+    if (!navOptions.includes(path)) path = '404';
 
     return (
         <div className='layout'>
@@ -17,7 +28,7 @@ const Layout = ({ children, location }) => {
                 <title>{path}</title>
             </Helmet>
             <div className='layout-content'>
-                <Header path={path} />
+                <Header path={path} navOptions={navOptions} />
                 {children}
             </div>
             <Footer />
