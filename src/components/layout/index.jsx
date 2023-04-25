fa71e220feef2ff5a69aa7ba2f94380c3571e45e
Tannor Breitigam <tannor@flowfi.com>
2023-04-24 21:00:25 -0700

    actually fix head properly

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "../../styles/index.css";
 import Footer from "../footer";
-import { Head } from "../head";
 import Header from "../header";
 import "./layout.css";
 
-export const Layout = ({ location, children }) => {
+export const Layout = ({ children }) => {
   return (
     <>
-      <Head location={location} />
       <div className="layout">
         <div className="layout-content">
           <Header />
