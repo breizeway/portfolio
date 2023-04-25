cb781a44a7f3ae386dee123e1fadf75bf03b5de0
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2023-04-24 21:00:54 -0700

    actually fix head properly (#53)

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -10,3 +10,4 @@ const NotFound = () => {
 };
 
 export default NotFound;
+export { Head } from "../components/head";
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,3 +11,4 @@ const Home = () => {
 };
 
 export default Home;
+export { Head } from "../components/head";
