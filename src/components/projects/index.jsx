1470cf9905318467f14f1fda90ee8fdbfa24e3b8
Tannor Breitigam <dev@tannor.net>
2023-05-31 12:11:13 -0700

    add projects component; add projects section to home page

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -12,7 +12,7 @@ export const Projects = ({}) => {
           <div className="project-image">
             <StaticImage
               src={`../../images/projects/song-bird.png`}
-              alt="SongBird website preview image showing yellow buttons and a text input with song lyrics"
+              alt="A screenshot of the SongBird website preview image showing yellow buttons and a text input with song lyrics"
             />
           </div>
         }
@@ -20,6 +20,45 @@ export const Projects = ({}) => {
         liveSiteUrl="https://songbird.tannor.net"
         repoUrl="https://github.com/breizeway/tab-lab"
       />
+      <Project
+        title="Google Meet Input Toggler"
+        image={
+          <div className="project-image">
+            <StaticImage
+              src={`../../images/projects/meet-toggler.png`}
+              alt="A screenshot of the Chrome extension store showing the Google Meet Input Toggler extension"
+            />
+          </div>
+        }
+        description="A Chrome extension to toggle your Google Meet microphone or camera with global shortcuts"
+        liveSiteUrl="https://chrome.google.com/webstore/detail/google-meet-input-toggler/hcbgpbodachfejmoecbbjnabdmigecnb"
+        repoUrl="https://github.com/breizeway/meet-toggler"
+      />
+      <Project
+        title="This site"
+        image={
+          <div className="project-image">
+            <StaticImage
+              src={`../../images/projects/portfolio.png`}
+              alt="A screenshot showing the site you're already on. Seems unnecessary."
+            />
+          </div>
+        }
+        description="A portfolio site to show off my skills and projects (You are here)"
+        liveSiteUrl="https://tannor.net"
+        repoUrl="https://github.com/breizeway/portfolio"
+      />
+      <div>
+        <h4>And more...</h4>
+        <p>
+          I'm working on new things all the time. Take a look at{" "}
+          <a href="https://github.com/breizeway?tab=repositories">
+            my Github repositories page
+          </a>{" "}
+          to see more (including an embarrassing array of old projects that
+          likely don't work anymore).
+        </p>
+      </div>
     </div>
   );
 };
