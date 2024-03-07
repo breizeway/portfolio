9900804dcd72ee1e01bcfd294e302a9a150f34fe
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2024-03-07 15:40:36 -0800

    another transition change

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("site-container").classList.remove("before-load");
+  setTimeout(() => {
+    document.getElementById("site-container").classList.remove("before-load");
+  });
 
   const contactForm = document.getElementById("contact-form");
   const formStatus = document.getElementById("contact-form__status");
