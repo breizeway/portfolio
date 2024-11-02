aa36ce9c57a5e0f927da7905a36ef50c8264c614
Tannor Breitigam <70126993+breizeway@users.noreply.github.com>
2024-11-02 15:26:03 -0700

    wc

--- scripts/index.js
+++ scripts/index.js
@@ -65,7 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("copyright-year").innerHTML =
     new Date().getFullYear();
 
-  document.getElementById("know-more-heading").onclick = () => {
+  document.getElementById("know-more").onclick = () => {
     document.getElementById("know-more-gif").classList.remove("hidden");
     setTimeout(() => {
       document.getElementById("know-more-gif").classList.add("hidden");
